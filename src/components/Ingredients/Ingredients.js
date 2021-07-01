9c6c9e9623688038b809032f8ce8ed75af594fc2
daky-2798 <dakshtandel.27@gmail.com>
2021-07-01 14:58:39 +0530

    useRef and Ref

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -8,23 +8,23 @@ const Ingredients = () => {
 
   const [userIngredient,setUserIngredient] = useState([]);
 
-  useEffect(() => {
-    fetch('https://reacts-hoks-update-default-rtdb.firebaseio.com/ingredient.json').then(
-      response => {
-        return response.json();
-      }
-    ).then(responseData => {
-      const loadedIngreient =[];
-      for(let key in responseData){
-        loadedIngreient.push({
-          id : key,
-          title : responseData[key].title,
-          amount : responseData[key].amount
-        });
-      }
-      setUserIngredient(loadedIngreient);
-    });
-  },[]);
+  // useEffect(() => {
+  //   fetch('https://reacts-hoks-update-default-rtdb.firebaseio.com/ingredient.json').then(
+  //     response => {
+  //       return response.json();
+  //     }
+  //   ).then(responseData => {
+  //     const loadedIngreient =[];
+  //     for(let key in responseData){
+  //       loadedIngreient.push({
+  //         id : key,
+  //         title : responseData[key].title,
+  //         amount : responseData[key].amount
+  //       });
+  //     }
+  //     setUserIngredient(loadedIngreient);
+  //   });
+  // },[]);
 
   const addIngredientHandler = ingredient => {
     fetch('https://reacts-hoks-update-default-rtdb.firebaseio.com/ingredient.json',{
@@ -40,14 +40,23 @@ const Ingredients = () => {
       ])
     });
   };
+  const setFilteredIngredient = useCallback(filteredIngredient => {
+    setUserIngredient(filteredIngredient);
+  },[]);
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredient(prevIngredients =>
+      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+    );
+  };
+
 
   return (
     <div className="App">
       <IngredientForm onAddIngredient = {addIngredientHandler} />
 
       <section>
-        <Search />
-        <IngredientList ingredients={userIngredient} onRemoveItem = {() => {}} />
+        <Search onLoadIngredient = {setFilteredIngredient} />
+        <IngredientList ingredients={userIngredient} onRemoveItem = {removeIngredientHandler} />
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const {onLoadIngredient} = props;
+  const [enteredFilter,setEnteredFilter] = useState('');
+  const inputRef = useRef();
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      if(enteredFilter === inputRef.current.value){
+        const query = enteredFilter.length === 0 
+        ? '' 
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+      fetch('https://reacts-hoks-update-default-rtdb.firebaseio.com/ingredient.json'+query).then(
+        response => {
+          return response.json();
+        }
+      ).then(responseData => {
+        const loadedIngreient =[];
+        for(let key in responseData){
+          loadedIngreient.push({
+            id : key,
+            title : responseData[key].title,
+            amount : responseData[key].amount
+          });
+        }
+        onLoadIngredient(loadedIngreient);
+      });
+      }
+    },500);
+    return () => {
+      clearTimeout(timer);
+    }
+  },[enteredFilter,onLoadIngredient,inputRef]);
+
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input ref={inputRef} type="text" value = {enteredFilter} 
+            onChange = {event => setEnteredFilter(event.target.value)} />
         </div>
       </Card>
     </section>
